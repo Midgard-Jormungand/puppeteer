a340cd798afc08422ff19831a8f264c8670bb74e
耶梦加得 <hgz983862527@bupt.edu.cn>
2019-11-07 14:06:10 +0800

    cam debug

--- client/src/main/scala/org/seekloud/puppeteer/client/model/RenderEngine.scala
+++ client/src/main/scala/org/seekloud/puppeteer/client/model/RenderEngine.scala
@@ -2,6 +2,8 @@ package org.seekloud.puppeteer.client.model
 
 import com.jme3.app.SimpleApplication
 import com.jme3.asset.plugins.FileLocator
+import com.jme3.input.KeyInput
+import com.jme3.input.controls.{ActionListener, KeyTrigger}
 import com.jme3.light.{AmbientLight, DirectionalLight}
 import com.jme3.math.{ColorRGBA, Quaternion, Vector3f}
 
@@ -12,6 +14,10 @@ import com.jme3.math.{ColorRGBA, Quaternion, Vector3f}
   * render2memory
   */
 object RenderEngine extends SimpleApplication {
+  private val KeyR = "keyR"
+
+  private val KeyT = "keyT"
+
   def gamePause(): Unit = enqueueToEngine({
     paused = true
   })
@@ -28,9 +34,10 @@ object RenderEngine extends SimpleApplication {
     /**
      * 摄像机
      */
-    cam.setLocation(new Vector3f(8.896082f, 12.328749f, 13.69658f))
-    cam.setRotation(new Quaternion(-0.09457599f, 0.9038204f, -0.26543108f, -0.32204098f))
+    cam.setLocation(new Vector3f(0, 7, 12))
+    cam.setRotation(new Quaternion(0f, 0.98957336f, -0.14402872f, 4.8319108E-4f))
     flyCam.setMoveSpeed(10f)
+    flyCam.setDragToRotate(true)
 
     /**
      * 要有光
@@ -38,6 +45,28 @@ object RenderEngine extends SimpleApplication {
     rootNode.addLight(new AmbientLight(new ColorRGBA(0.2f, 0.2f, 0.2f, 1f)))
     rootNode.addLight(new DirectionalLight(new Vector3f(-1, -2, -3), new ColorRGBA(0.8f, 0.8f, 0.8f, 1f)))
 
+    inputManager.addMapping(KeyR, new KeyTrigger(KeyInput.KEY_R))
+    inputManager.addMapping(KeyT, new KeyTrigger(KeyInput.KEY_T))
+    inputManager.addListener(actionListener, KeyR, KeyT)
+
+  }
+
+  private val actionListener = new ActionListener() {
+    override def onAction(name: String, isPressed: Boolean, tpf: Float): Unit = {
+      if (isPressed) name match {
+        case KeyR =>
+          val location = cam.getLocation
+          val rotation = cam.getRotation
+          println(location + "," + rotation)
+        case KeyT =>
+          if(flyCam.isDragToRotate){
+            flyCam.setDragToRotate(false)
+          }else{
+            flyCam.setDragToRotate(true)
+          }
+        case _ =>
+      }
+    }
   }
 
   /**
